ae7f0a01af7267a19a1b4e61ca55b90e4b24bb6b
Michael Diep <michaeldiep97@gmail.com>
2018-08-27 00:13:43 -0500

    Fixed broken navbar dropdowns

---
+++ js/stickyNav.js
@@ -0,0 +1,19 @@
+/* script taken and modified from https://www.w3schools.com */
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var nav = document.getElementById("nav")
+
+// Get the offset position of the navbar
+var sticky = nav.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky");
+  }
+} 
